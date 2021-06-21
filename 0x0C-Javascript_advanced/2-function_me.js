daa5324eb6d3320aae432265e64b5b91cb583860
onsjannet <onsjannet@gmail.com>
2021-06-21 19:29:24 +0100

    alert

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,10 @@
+// a function that displays 3 alerts
+
+function welcomeMessage(fullName){
+	alert('welcome' + fullName);
+}
+
+guillame = function() {welcomeMessage(" "+"Guillaume")};
+alex = function() {welcomeMessage(" "+"Alex")};
+fred = function() {welcomeMessage(" "+"Fred")};
+
