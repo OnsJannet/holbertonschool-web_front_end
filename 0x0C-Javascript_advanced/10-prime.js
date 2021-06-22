5b32f59eb72f181c710ef71e9e0013997f6b3995
onsjannet <onsjannet@gmail.com>
2021-06-22 20:30:35 +0100

    100 times

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,25 @@
+
+
+function countPrimeNumbers(number){
+	for (let counter = 0; counter <= 100; counter++) {
+		let notPrime = false;
+		for (let i = 2; i < number; i++) {
+        	if (counter%i===0 && i!==counter) {
+				notPrime = true;
+			}
+		}
+		if (notPrime === false) {
+                console.log(counter);
+		}
+	}
+}
+
+
+let t0 = performance.now();
+var times = 100;
+for (var i=0; i < times; i++){
+	countPrimeNumbers();
+}
+let t1 = performance.now();
+
+console.log(`Execution time of printing countPrimeNumbers was ${(t1- t0)/100} milliseconds`);
