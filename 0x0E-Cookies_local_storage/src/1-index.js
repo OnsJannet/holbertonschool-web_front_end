19b2e40111f612cefce6e2c0b71ad3f881d47203
onsjannet <onsjannet@gmail.com>
2021-06-29 18:54:18 +0100

    cookies with expiry date

---
+++ 0x0E-Cookies_local_storage/src/1-index.js
@@ -0,0 +1,14 @@
+// Creates cookies that expires in 10 days
+
+function setCookies(){
+    document.cookie= `firstname=${document.getElementById('firstname').value}`;
+    document.cookie= `email=${document.getElementById('email').value}`;
+}
+
+function showCookies() {
+    const paragraph = document.createElement('p');
+    const cookies = `Cookies: ${decodeURIComponent(document.cookie)}`
+    const text= document.createTextNode(cookies);
+    paragraph.appendChild(text);
+    document.body.appendChild(paragraph);
+}
